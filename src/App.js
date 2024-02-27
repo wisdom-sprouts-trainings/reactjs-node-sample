1db5fc50e461ce7e54bbc785711a6b7b83153ae6
websatva-technologies <69213223+websatva-technologies@users.noreply.github.com>
2024-02-27 11:25:29 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { ListContainer } from './ListContainer';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [students, setStudents] = useState([]);
+
+  async function fetchStudents() {
+    try {
+      const response = await fetch("http://localhost:3011/api/student", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const studentResponse = await response.json();
+
+      console.log(studentResponse);
+
+      setStudents(studentResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-50 mx-auto">
+      <ListContainer students = {students}/>
     </div>
   );
 }
